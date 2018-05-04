cfc93fecbda920d9ef85f93f6852a34c3fd8d60d
ITwrx <ITwrx@users.noreply.github.com>
2018-05-04 10:58:57 -0500

    Update CensorMiddleware.php
    fixes https://github.com/kamranahmedse/laravel-censor/issues/2 for my use case. 'replace' functionality is not reimplemented in this refactor, as i only needed 'redact' functionality.

--- src/CensorMiddleware.php
+++ src/CensorMiddleware.php
@@ -39,7 +39,10 @@ class CensorMiddleware
         $this->prepareDictionary();
 
         $content = $response->getContent();
+
         $content = $this->censorResponse($content);
+
+        //dd($content);
 
         $response->setContent($content);
 
@@ -53,6 +56,7 @@ class CensorMiddleware
     private function prepareDictionary()
     {
         $replaceDict = Config::get('censor.replace', []);
+
         $redactDict  = Config::get('censor.redact', []);
 
         $replaceDictKeys   = array_keys($replaceDict);
@@ -94,15 +98,39 @@ class CensorMiddleware
 
         // Word boundary and word matching regex
         $replaceables = '\b' . implode('\b|\b', $replaceables) . '\b';
-        $regex        = '/>(?:[^<]*?(' . $replaceables . ')[^<]*?)</i';
+        //$regex        = '/>(?:[^<]*?(' . $replaceables . ')[^<]*?)</i';
+        $regex        = '(' . $replaceables . ')';
 
         // Make the keys lower case so that it is easy to lookup
         // the replacements
         $toReplace = array_change_key_case($this->replaceDict, CASE_LOWER);
         $toRedact  = $this->redactDict;
 
+        $content = preg_match_all( $regex, $source, $matches );
+
+        $matches = array_flatten($matches);
+
+        $i = 0;
+
+        foreach ($matches as $match) {
+
+            $temp = strtolower($match);
+
+            //if ($this->_inArray($temp, $toRedact) || $this->getRedactRegexKey($temp)) {  // If it matches a word or pattern to redact
+
+            $replaceWith = str_repeat('*', strlen($temp));
+
+            $source = str_replace($match, $replaceWith, $source);
+
+            //}
+
+            $i++;
+        }
+
+        return $source;
+
         // Find all the matches and keep redacting/replacing
-        $source = preg_replace_callback($regex, function ($match) use ($toReplace, $toRedact) {
+        /*$source = preg_replace_callback($regex, function ($match) use ($toReplace, $toRedact) {
 
             $temp = strtolower($match[1]);
 
@@ -113,15 +141,16 @@ class CensorMiddleware
                 return str_replace($match[1], $toReplace[$regexKey], $match[0]);
             } elseif ($this->_inArray($temp, $toRedact) || $this->getRedactRegexKey($temp)) {  // If it matches a word or pattern to redact
                 $replaceWith = str_repeat('*', strlen($temp));
-
                 return str_replace($match[1], $replaceWith, $match[0]);
+
             } else {
                 return $match[0];
             }
 
         }, $source);
 
-        return $source;
+
+        return $source;*/
     }
 
     /**
@@ -132,7 +161,7 @@ class CensorMiddleware
      *
      * @return bool|string
      */
-    public function getReplaceRegexKey($matched)
+   /* public function getReplaceRegexKey($matched)
     {
         foreach ($this->replaceDict as $pattern => $replaceWith) {
             if (preg_match('/' . $pattern . '/', $matched)) {
@@ -141,7 +170,7 @@ class CensorMiddleware
         }
 
         return false;
-    }
+    }*/
 
     /**
      * Case in-sensitive in_array
@@ -151,10 +180,10 @@ class CensorMiddleware
      *
      * @return bool
      */
-    private function _inArray($needle, $haystack)
+    /*private function _inArray($needle, $haystack)
     {
         return in_array(strtolower($needle), array_map('strtolower', $haystack));
-    }
+    }*/
 
     /**
      * Gets the matched item and check if it matches any of the patterns
@@ -164,7 +193,7 @@ class CensorMiddleware
      *
      * @return bool
      */
-    public function getRedactRegexKey($matched)
+   /* public function getRedactRegexKey($matched)
     {
         foreach ($this->redactDict as $pattern) {
             if (preg_match('/' . $pattern . '/', $matched)) {
@@ -173,5 +202,6 @@ class CensorMiddleware
         }
 
         return false;
-    }
+    }*/
 }
+
